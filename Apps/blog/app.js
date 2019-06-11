8cad6eeb073f4c0b382944a9c0443b061ed924c2
Thomas Belloc <thomas.belloc@equadex.net>
2019-06-11 17:14:47 +0200

    squelette blog

--- Apps/blog/app.js
+++ Apps/blog/app.js
@@ -15,7 +15,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req,res) {
+  res.render("home", { startingContent: homeStartingContent});
+})
 
+app.get("/about", function(req,res) {
+  res.render("about", { aboutContent: aboutContent});
+})
+
+app.get("/contact", function(req,res) {
+  res.render("contact", { contactContent: contactContent});
+})
 
 
 

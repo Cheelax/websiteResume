5214a8c2f91784e9662dc78c32bda1c9838bd74d
Thomas Belloc <thomas.belloc@equadex.net>
2019-06-12 13:11:35 +0200

    wip

--- Apps/blog/app.js
+++ Apps/blog/app.js
@@ -51,17 +51,6 @@ app.post("/posts/:postName", function(req,res) {
   console.log(req.params.postName)
 })
 
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
